c3a32b8872da9e2dad3ebea1cdc0c45a8969d8fd
Jason Chauviere <jason.chauviere@etu.univ-nantes.fr>
2019-04-14 21:23:15 +0200

    Question 1 bis

--- ex5-ini/mailreader/src/test/java/com/acme/mailreader/bdd/MailComparaisonStep.java
+++ ex5-ini/mailreader/src/test/java/com/acme/mailreader/bdd/MailComparaisonStep.java
@@ -14,6 +14,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
